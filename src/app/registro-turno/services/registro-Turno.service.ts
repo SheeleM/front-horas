21ed20795e745fdc94a115ab1f4779997b1c2ba4
SheeleM <sulay.martinez@udea.edu.co>
2025-04-15 21:33:32 -0500

    se termina el front usuarioturno

--- src/app/registro-turno/services/registro-Turno.service.ts
+++ src/app/registro-turno/services/registro-Turno.service.ts
@@ -1,6 +1,8 @@
+import { CreateUsuarioTurnoDto, RegistroTurnoComponent } from './../registro-turno.component';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { Ingeniero } from '../registro-turno.component';
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +14,22 @@ export class RegistroTurnoService {
   getAllTurno(){
     return this.http.post(`${environment.url}turno/codigo`,{})
   }
+
+  getAllUsuarios(){
+   /// return this.http.post(`${environment.url}user/fullname`,{})
+   return this.http.post<Ingeniero[]>(`${environment.url}user/fullname`, {});
+
+  }
+
+  CreatedUsuarioTurno(){
+    /// return this.http.post(`${environment.url}user/fullname`,{})
+    return this.http.post<Ingeniero[]>(`${environment.url}usuario-turno`, {});
+ 
+   }
+
+   crearUsuarioTurno(dto: CreateUsuarioTurnoDto) {
+    return this.http.post(`${environment.url}usuario-turno`, dto);
+  }
+
+  
 }
